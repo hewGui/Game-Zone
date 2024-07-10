d05dd6acfa4a5de918dcbb834eafa19950084fc4
Guilherme <guilhermesoaresilvat@gmail.com>
2024-07-10 18:08:34 -0300

    Criação do BackEnd da loja Game Zone

---
+++ gamezone/src/main/java/com/generation/gamezone/GamezoneApplication.java
@@ -0,0 +1,13 @@
+package com.generation.gamezone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamezoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamezoneApplication.class, args);
+	}
+
+}
---
+++ gamezone/src/test/java/com/generation/gamezone/GamezoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.gamezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamezoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
